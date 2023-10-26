39fb1b4bc4767845bc714f07abf50ef87bbbcade
Rosh-Cherian <98978927+Rosh-Cherian@users.noreply.github.com>
2023-10-27 03:51:58 +0530

    Add files via upload
    Finalised but have some problems with buttons when re-entering data

--- script.js
+++ script.js
@@ -2,10 +2,12 @@ document.addEventListener("DOMContentLoaded", function() {
     const showFormButton = document.getElementById("showFormButton");
     const showSecondFormButton = document.getElementById("showSecondFormButton");
     const detailsForm = document.getElementById("detailsForm");
+    const details2Form = document.getElementById("details2Form");
+    //const secondFormContainer = document.getElementById("secondFormContainer");
+    const inputForm = document.getElementById("inputForm");
     const secondForm = document.getElementById("secondForm");
     const submittedDetails = document.getElementById("submittedDetails");
     const submittedText = document.getElementById("submittedText");
-    const secondFormContainer = document.getElementById("secondFormContainer");
   
     showFormButton.addEventListener("click", function() {
       detailsForm.classList.remove("hidden");
@@ -13,8 +15,31 @@ document.addEventListener("DOMContentLoaded", function() {
     });
   
     showSecondFormButton.addEventListener("click", function() {
-      secondFormContainer.classList.remove("hidden");
+      details2Form.classList.remove("hidden");
       showSecondFormButton.classList.add("hidden");
+    });
+  
+    inputForm.addEventListener("submit", function(event) {
+      event.preventDefault(); // Prevent the default form submission behavior
+  
+      const field1Input = document.getElementById("field1").value;
+      const field2Input = document.getElementById("field2").value;
+      const field3Input = document.getElementById("field3").value;
+      const field4Input = document.getElementById("field4").value;
+  
+      // Process and display the first form data
+      const submittedTextContent = `
+        Task Name: ${field1Input}<br>
+        Task Description: ${field2Input}<br>
+        Deadline: ${field3Input}<br>
+        Domain: ${field4Input}
+      `;
+  
+      submittedText.innerHTML = "Task submitted:<br>" + submittedTextContent;
+  
+      submittedDetails.classList.remove("hidden");
+      inputForm.classList.add("hidden");
+      showFormButton.classList.remove("hidden");
     });
   
     // Event listener for the second form
@@ -26,14 +51,15 @@ document.addEventListener("DOMContentLoaded", function() {
   
       // Process and display the second form data
       const submittedTextContent = `
-        Field 5: ${field5Input}<br>
-        Field 6: ${field6Input}
+        Name: ${field5Input}<br>
+        Domain: ${field6Input}
       `;
   
-      submittedText.innerHTML = "Details submitted:<br>" + submittedTextContent;
+      submittedText.innerHTML = "Employee Details submitted:<br>" + submittedTextContent;
   
       submittedDetails.classList.remove("hidden");
       secondFormContainer.classList.add("hidden");
+      showSecondFormButton.classList.remove("hidden");
     });
   });
   
